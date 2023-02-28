fdd838f40fdb4ca45ac63287ad38e83f2bf9af8a
Zoe Delap <73131522+zoedelap@users.noreply.github.com>
2023-02-28 13:25:22 -0600

    now it makes colorful spheres as well

--- Mod05a-Delap/Assets/CubeSpawner.cs
+++ Mod05a-Delap/Assets/CubeSpawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CubeSpawner : MonoBehaviour
 {
     public GameObject cubePrefabVar;
+    public GameObject spherePrefabVar;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +19,11 @@ public class CubeSpawner : MonoBehaviour
     {
         // Instantiate(cubePrefabVar);
         GameObject cubeGO = Instantiate(cubePrefabVar);
-        Material mat = cubeGO.GetComponent<Renderer>().material;
-        mat.color = Random.ColorHSV(0, 1, 0.5f, 1, 0.75f, 1);
+        Material cubeMat = cubeGO.GetComponent<Renderer>().material;
+        cubeMat.color = Random.ColorHSV(0, 1, 0.5f, 1, 0.75f, 1);
+
+        GameObject sphereGO = Instantiate(spherePrefabVar);
+        Material sphereMat = sphereGO.GetComponent<Renderer>().material;
+        sphereMat.color = Random.ColorHSV(0, 1, 0.5f, 1, 0.75f, 1);
     }
 }
